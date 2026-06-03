67676078dae3bbdbd97e3f7722c60193a37ddcd7
LogicLover50 <200545448+LogicLover50@users.noreply.github.com>
2026-06-03 20:56:43 +0700

    Fix Build & Run Settings

--- Jokers/IScore.h
+++ Jokers/IScore.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "C:\Users\Gaming 3\Documents\2gas\Kuliah\Design Pattern\Revised\DP-5224600076-5224600078-Hans-Irgi-main\ChosenHand.h"
+#include "..\ChosenHand.h"
 
 // Interface dasar Decorator yang kini menerima data ChosenHand
 class IScore {
